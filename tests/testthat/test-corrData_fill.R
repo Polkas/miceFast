3b1ec85c7f4c773cc0526205ba0da692c1c0aa6b
Maciej Nasinski <nasinski.maciej@gmail.com>
2018-02-24 00:09:44 +0100

    testthat

--- tests/testthat/test-corrData_fill.R
+++ tests/testthat/test-corrData_fill.R
@@ -2,10 +2,51 @@
 context("corrData-fill")
 
 
-# test_that("fill-data",
-#           {
-#
-#             expect_true()
-#
-#           })
+test_that("fill-data",
+          {
+            set.seed(1234)
+
+            #parameters
+
+            power = 4 # power of 10 - number of observations - should be adjusted to a computer capabilities
+
+            nr_var = 7 #CHANGE - only if you generate a bigger corr matrix:  number of variables - independent and one dependent
+
+            grs = max(c(10**(power-3),10)) # grouping variable - number of groups
+
+            iters = 10 # number of iterations for benchmarking
+
+            ## generete example - data
+
+            ##positive-defined correlation matrix
+
+            cors = matrix(c(1,0.6,0.7,0.4,0.4,0.5,0.35,
+                            NA,1,0.2,0.05,0.1,0.12,0.15,
+                            NA,NA,1,0.15,0.15,0.1,0.08,
+                            NA,NA,NA,1,0.12,0.15,0.1,
+                            NA,NA,NA,NA,1,0.15,0.2,
+                            NA,NA,NA,NA,NA,1,0.15,
+                            NA,NA,NA,NA,NA,NA,1),7,7,byrow = T)
+
+            cors[lower.tri(cors)] = t(cors)[lower.tri(cors)]
+
+            # automatic corr matrix - close to diagonal
+
+            #cors = stats::rWishart(100,nr_var+10,diag(nr_var))
+
+            #cors = apply(cors,1:2,mean)/(nr_var+10)
+
+            #cors
+
+            ##
+
+            model = new(corrData,10,10^power,rep(0,nr_var),cors)
+
+            data_bin = model$fill("binom")
+            data_disc = model$fill("discrete")
+            data_con = model$fill("contin")
+
+            expect_true(all())
+
+          })
 
